f68325b92e31bee1313e16dd42446bc3281fe872
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-02-05 14:52:21 +0200

    sum validation added

--- src/main/java/codewithfreddy/ComplexJsonParse.java
+++ src/main/java/codewithfreddy/ComplexJsonParse.java
@@ -9,8 +9,8 @@ public class ComplexJsonParse {
         JsonPath js=new JsonPath(payload.CoursePrice());
 
         //Print No of courses returned by API
-        int courseCount = js.getInt("courses.size()");
-        System.out.println("Total courses:" + courseCount);
+        int count = js.getInt("courses.size()");
+        System.out.println("Total courses:" + count);
 
         //Print Purchase Amount
         int totalAmount = js.getInt("dashboard.purchaseAmount");
@@ -21,7 +21,7 @@ public class ComplexJsonParse {
         System.out.println(titleFirstCourse);
 
         //Print All course titles and their respective Prices
-        for(int i=0;i<courseCount;i++)
+        for(int i=0;i<count;i++)
         {
             String courseTitles=js.get("courses["+i+"].title");
             System.out.println(courseTitles);
@@ -34,7 +34,7 @@ public class ComplexJsonParse {
 
         System.out.println("Print no of copies sold by RPA Course");
 
-        for(int i=0;i<courseCount;i++) {
+        for(int i=0;i<count;i++) {
             String courseTitles = js.get("courses[" + i + "].title");
             if (courseTitles.equalsIgnoreCase("RPA")) {
                 int copies = js.get("courses[" + i + "].copies");
