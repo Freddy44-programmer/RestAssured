f08ef78ec781721d20a7111e94b01b8ff18395ac
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-02-08 20:40:51 +0200

    data provider added

--- src/main/java/codewithfreddy/DynamicJson.java
+++ src/main/java/codewithfreddy/DynamicJson.java
@@ -4,22 +4,29 @@ import files.ReUsableMethods;
 import files.payload;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class DynamicJson {
 
-    @Test
-    public void addBook(){
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle ){
         RestAssured.baseURI="http://216.10.245.166";
        String resp = given().header("Content-Type", "application/json")
-                .body(payload.AddBook("adsfs","6463")).when()
+                .body(payload.AddBook(isbn, aisle)).when()
                 .post("Library/Addbook.php")
                 .then().assertThat().statusCode(200)
                 .extract().response().asString();
           JsonPath js = ReUsableMethods.rawToJson(resp);
           String id = js.get("ID");
         System.out.println(id);
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData(){
+
+        return new Object[][] {{"rereh", "4534"},{"dfdf", "5454"},{"fgdd", "3453"}};
     }
 }
